3e26b54530ee83fcc7845cabb7db6237c7aa04f9
Guilherme Sette <guilherme.sette@codus.com.br>
2019-09-30 13:51:40 -0300

    add bownes of classes
    Signed-off-by: Guilherme Sette <guilherme.sette@codus.com.br>

---
+++ CalculadoraPolinomios/src/calculadorapolinomio/CalculadoraPolinomio.java
@@ -0,0 +1,26 @@
+package calculadorapolinomio;
+
+/**
+ *
+ * @author Guilherme Sette - 41783441
+ * @author Luiz Henrique Monteiro de Carvalho - 41719468
+ *
+ */
+
+public class CalculadoraPolinomio {
+
+    private Polinomio Polinomio_1;
+    private Polinomio Polinomio_2;
+
+    public CalculadoraPolinomio(Polinomio Polinomio_1, Polinomio Polinomio_2){
+        this.Polinomio_1 = Polinomio_1;
+        this.Polinomio_2 = Polinomio_2;
+    }
+
+    public static void main(String[] args) {
+        Polinomio polinomio_1;
+        Polinomio polinomio_2;
+        CalculadoraPolinomio calculadora;
+    }
+}
+
---
+++ CalculadoraPolinomios/src/calculadorapolinomio/Polinomio.java
@@ -0,0 +1,16 @@
+package calculadorapolinomio;
+
+public class Polinomio {
+    // vetor para representar os termos do polinomio
+    private double[] termos;
+    private int grau;
+    // recebe um grau de polinômio e instancia os termos do
+    // polinômio
+    public Polinomio(int grau){
+
+    }
+    // imprime o polinômio, será explica a seguir
+    public void mostra(){
+
+    }
+}
