2bb659654c2a08eb443f2f8af69cfe3a8f2ded2e
Guilherme Sette <guilherme.sette@codus.com.br>
2019-09-30 18:07:14 -0300

    create menu and methods
    Signed-off-by: Guilherme Sette <guilherme.sette@codus.com.br>

--- CalculadoraPolinomios/src/calculadorapolinomio/CalculadoraPolinomio.java
+++ CalculadoraPolinomios/src/calculadorapolinomio/CalculadoraPolinomio.java
@@ -1,4 +1,6 @@
 package calculadorapolinomio;
+import java.util.Scanner;
+
 
 /**
  *
@@ -18,8 +20,8 @@ public class CalculadoraPolinomio {
     }
 
     //Inverte vetor
-    public static int[] inverterVetor(int vet[]) {
-        int temp;
+    private static double[] inverterVetor(double vet[]) {
+        double temp;
         for (int i = 0; i < vet.length; i++) {
             for (int j = 0; j < i; j++) {
                 temp = vet[i];
@@ -30,12 +32,85 @@ public class CalculadoraPolinomio {
         return vet;
     }
 
+    public static double[] preenche_vetor(int grau){
+        double[] vetor = new double[grau + 1];
+        Scanner input = new Scanner(System.in);
+        for (int i =0; i < grau + 1; i++) {
+            if (i == (grau - 1)) {
+                vetor[i] = 0;
+            } else {
+                System.out.print("Digite o valor\n");
+                vetor[i] = input.nextInt();
+            }
+        }
+        return vetor;
+    }
+
     public static void main(String[] args) {
         Polinomio polinomio_1;
         Polinomio polinomio_2;
         CalculadoraPolinomio calculadora;
 
-        
+        //Menu
+        Scanner input = new Scanner(System.in);
+        boolean mainLoop = true;
+
+        int escolha;
+        while(true){
+            System.out.println("----------------------\n");
+            System.out.println("Menu - Calculadora\n");
+            System.out.print("1.) Criar Polinômio \n");
+            System.out.print("2.) Somar\n");
+            System.out.print("3.) Multiplicar\n");
+            System.out.print("0.) Exit\n");
+            System.out.print("\nDigite sua opção: ");
+            System.out.println("----------------------\n");
+
+            escolha = input.nextInt();
+
+            switch(escolha){
+
+                case 1:
+                    //Criando polinomio
+                    int grau_polinomio_1, grau_polinomio_2;
+                    double[] vetor_termos_1;
+                    double[] vetor_termos_2;
+                    System.out.print("Digite o grau do polinômio 1: ");
+                    grau_polinomio_1 = input.nextInt();
+                    polinomio_1 = new Polinomio(grau_polinomio_1);
+                    vetor_termos_1 = new double[grau_polinomio_1];
+                    //Preencher polinomio
+                    vetor_termos_1 = preenche_vetor(grau_polinomio_1);
+                    polinomio_1.preenche_termos(inverterVetor(vetor_termos_1));
+                    System.out.print("\nPolinômio 1 criado com sucesso!");
+
+                    System.out.print("\nDigite o grau do polinômio 2: ");
+                    grau_polinomio_2 = input.nextInt();
+                    polinomio_2 = new Polinomio(grau_polinomio_2);
+                    vetor_termos_2 = new double[grau_polinomio_2];
+                    //Preencher polinomio
+                    vetor_termos_2 = preenche_vetor(grau_polinomio_2);
+                    polinomio_2.preenche_termos(inverterVetor(vetor_termos_2));
+                    System.out.print("\nPolinômio 2 criado com sucesso!");
+                    break;
+
+                case 2:
+                    System.out.println("Implementar Soma");
+                    break;
+
+                case 3:
+                    System.out.println("Implementar Multiplicação");
+                    break;
+
+                case 0:
+                    System.out.println("Exiting Program...");
+                    System.exit(0);
+                    break;
+
+                default :
+                    System.out.println("This is not a valid Menu Option! Please Select Another");
+                    break;
+            }
+        }
     }
 }
-
--- CalculadoraPolinomios/src/calculadorapolinomio/Polinomio.java
+++ CalculadoraPolinomios/src/calculadorapolinomio/Polinomio.java
@@ -10,15 +10,13 @@ public class Polinomio {
         this.grau = grau;
     }
 
-    public Polinomio preenche_termos(double[] termos){
+    public void preenche_termos(double[] termos){
         for (int i =0; i < this.grau + 1; i++){
             this.termos[i] = (i == (this.grau - 1)) ?  0 : termos[i];
         }
     }
 
-
     // imprime o polinômio, será explica a seguir
     public void mostra(){
-
     }
 }
